34b2bdfb59d9bf57c477b88d229225f5eac6b049
Felix Müller <felix.mueller@dm.de>
2025-05-28 23:14:39 +0200

    Disables CORS in local development

---
+++ src/main/java/de/fmm/recipestore/api/LocalDevelopmentCorsConfig.java
@@ -0,0 +1,25 @@
+package de.fmm.recipestore.api;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Profile("local")
+public class LocalDevelopmentCorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull final CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
